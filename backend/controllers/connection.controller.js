713a19880380736bb57843428127bae5dd1f22da
Youngjae <2015fromjeki@gmail.com>
2025-05-28 20:11:31 +1000

    Feat : Implement all backend

--- backend/controllers/connection.controller.js
+++ backend/controllers/connection.controller.js
@@ -103,16 +103,130 @@ export const acceptConnectionRequest = async (req, res) => {
     return res.json({ message: "Connection accepted successfully" });
   } catch (error) {
     console.error("Error in acceptConnectionRequest", error.message);
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: "Internal Error in acceptConnectionRequest",
-      });
+    return res.status(500).json({
+      success: false,
+      message: "Internal Error in acceptConnectionRequest",
+    });
   }
 };
-export const rejectConnectionRequest = (req, res) => {};
-export const getUserConnections = (req, res) => {};
-export const getConnectionRequests = (req, res) => {};
-export const removeConnection = (req, res) => {};
-export const getConnectionStatus = (req, res) => {};
+export const rejectConnectionRequest = async (req, res) => {
+  try {
+    const { requestId } = req.params;
+    const userId = req.user._id;
+
+    const request = await ConnectionRequest.findById(requestId);
+
+    if (request.recipient.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to reject this request" });
+    }
+
+    if (request.status !== "pending") {
+      return res
+        .status(400)
+        .json({ message: "This request has already been processed" });
+    }
+
+    // DB 저장하기전에 바꿔주기
+    request.status = "rejected";
+    await request.save();
+
+    return res.status(200).json({ message: "Connection request rejected" });
+  } catch (error) {
+    console.error("Error in rejectConnectionRequest", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Error in rejectConnectionRequest",
+    });
+  }
+};
+// Need to be implemented
+export const getConnectionRequests = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const request = await ConnectionRequest.find({
+      recipient: userId,
+      status: "pending",
+    }).populate("sender", "name username profilePicture headline connections");
+
+    return res.status(200).json(request);
+  } catch (error) {
+    console.error("Error in getConnectionRequests", error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Error in getConnection Requests" });
+  }
+};
+//
+export const getUserConnections = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    // Connection user Chaining해서 가져오기
+    // 가져와서 정보 rendering할것을 생각해야함........
+    const user = await User.findById(userId).populate(
+      "connections",
+      "name username profilePicture headline connections"
+    );
+    return res.json(user.connections);
+  } catch (error) {
+    console.error("Error in getUserConnections", error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Error in getUserConnections" });
+  }
+};
+export const removeConnection = async (req, res) => {
+  try {
+    const myID = req.user._id;
+    const { userId } = req.params;
+
+    // 서로 목록에서 지워주기
+    await User.findByIdAndUpdate(myID, { $pull: { connections: userId } }),
+      await User.findByIdAndUpdate(userId, { $pull: { connections: myID } });
+
+    return res.json({ message: "Connection removed successfully" });
+  } catch (error) {
+    console.error("Error in removeConnection", error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Error in removeConnection Requests" });
+  }
+};
+export const getConnectionStatus = async (req, res) => {
+  try {
+    const targetUserId = req.params.userId;
+    const currentUserId = req.user._id;
+
+    const currentUser = req.user;
+
+    // They are already connected
+    if (currentUser.connections.includes(targetUserId)) {
+      return res.json({ status: "connected" });
+    }
+
+    const pendingRequest = await ConnectionRequest.findOne({
+      $or: [
+        { sender: currentUserId, recipient: targetUserId },
+        { sender: targetUserId, recipient: currentUserId },
+      ],
+      status: "pending",
+    });
+
+    if (pendingRequest) {
+      if (pendingRequest.sender.toString() === currentUserId.toString()) {
+        return res.json({ status: "pending" });
+      } else {
+        return res.json({ status: "received", requestId: pendingRequest._id });
+      }
+    }
+    // if no connection or pending req found
+    return res.json({ status: "not connected" });
+  } catch (error) {
+    console.error("Error in getConnectionStatus", error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Error in getConnectionStatus " });
+  }
+};
