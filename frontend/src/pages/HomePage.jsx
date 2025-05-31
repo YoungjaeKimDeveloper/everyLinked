2fb98b125fe044f0647e0a69c92da16ce244a8ad
Youngjae <2015fromjeki@gmail.com>
2025-05-31 10:07:58 +1000

    Feat: Implement Set up

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 // Components
 import PostCreation from "../components/home/PostCreation.jsx"
 import Sidebar from "../components/home/Sidebar.jsx"
+import Post from "../components/home/Post.jsx"
+import RecommendedUser from "../components/home/RecommendedUser.jsx"
 // Internal Library
 import { axiosInstance } from "../lib/axios.js"
 // External Library
 import { useQuery } from "@tanstack/react-query"
+import { Users } from "lucide-react"
 import toast from "react-hot-toast"
 
 const HomePage = () => {
@@ -46,6 +49,29 @@ const HomePage = () => {
             </div>
             <div className='col-span-1 lg:col-span-2 order-first lg:order-none'>
                 <PostCreation user={authUser} />
+                {/* POST CARD */}
+                {posts?.map(post => <Post key={post._id} post={post} />)}
+                {/* When there is no post */}
+                {posts?.length === 0 && (
+                    <div className='bg-white rounded-lg shadow p-8 text-center'>
+                        <div className='mb-6'>
+                            <Users size={64} className='mx-auto text-blue-500' />
+                        </div>
+                        <h2 className='text-2xl font-bold mb-4 text-gray-800'>No Posts Yet</h2>
+                        <p className='text-gray-600 mb-6'>Connect with others to start seeing posts in your feed!</p>
+                    </div>
+                )}
+                {/* Recommended users */}
+                {recommendedUsers?.length > 0 && (
+                    <div className='col-span-1 lg:col-span-1 hidden lg:block'>
+                        <div className='bg-secondary rounded-lg shadow p-4'>
+                            <h2 className='font-semibold mb-4'>People you may know</h2>
+                            {recommendedUsers?.map((user) => (
+                                <RecommendedUser key={user._id} user={user} />
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
